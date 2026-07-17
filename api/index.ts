76c030bac430838a333f15307048c08f93b9b3b8
kokosabar <faisalsabaryanto44@gmail.com>
2026-07-18 01:32:58 +0700

    fix: remove api/index.ts to resolve Vercel conflicting paths error

--- api/index.ts
+++
@@ -1,3 +0,0 @@
-// This file is compiled to api/index.js by `npm run build:api`
-// Vercel uses the compiled api/index.js as the serverless function entry point.
-export { default } from "./_handler";
